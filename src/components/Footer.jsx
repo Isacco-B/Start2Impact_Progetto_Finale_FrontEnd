703fffd11eee4eac4ffa0a01b83df018d1acb18e
Isacco-B <git@isaccobertoli.com>
2024-02-14 23:19:03 +0100

    complete footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,26 @@
+import { socialLinks } from "../constants";
+
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="w-full py-6">
+      <div className="flex flex-col gap-4 justify-center items-center md:flex-row md:justify-around">
+        <p className="text-sm md:text-[16px] text-slate-600">
+          © 2024, made with 💘 by <strong>Isacco Bertoli</strong> for <strong>Start2Impact.</strong>
+        </p>
+        <div className="flex gap-8">
+          {socialLinks.map((link) => (
+            <a
+              href={link.href}
+              key={link.id}
+              target="_blank"
+              rel="noreferrer"
+              className="text-slate-600 hover:text-slate-800"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
